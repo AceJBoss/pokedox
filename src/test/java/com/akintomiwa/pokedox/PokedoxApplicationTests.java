c72bf2d1cb39f8d4ecc4bb791a853ac443cf05c2
Akintomiwa Abolade <jboss@Akintomiwas-MacBook-Pro.local>
2022-01-06 21:57:53 +0100

    Pokedox app setup

---
+++ src/test/java/com/akintomiwa/pokedox/PokedoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akintomiwa.pokedox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
