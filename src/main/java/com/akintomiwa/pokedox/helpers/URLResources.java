c740200b45c783f234b8ef3d3c84413faa16d379
Akintomiwa Abolade <jboss@Akintomiwas-MacBook-Pro.local>
2022-01-09 10:57:17 +0100

    Helper Methods

---
+++ src/main/java/com/akintomiwa/pokedox/helpers/URLResources.java
@@ -0,0 +1,10 @@
+package com.akintomiwa.pokedox.helpers;
+
+public class URLResources {
+    public static final String V2_GET_POKEMON = "/api/v2/pokemon-species/{pokemonName}";
+    public static final String V2_GET_POKEMON_EMPTY_PARAM = "/api/v2/pokemon-species/";
+    public static final String TRANSLATE = "/translate/";
+    public static final String YODA = "yoda";
+    public static final String SHAKESPEARE = "shakespeare";
+    public static final String DOT_JSON = ".json";
+}
